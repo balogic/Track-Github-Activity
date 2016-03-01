c05ad5cca5b8f0978d20c1cb23762c8dc7ff4e68
Balaji <rbalajives@gmail.com>
2016-03-01 16:35:31 +0530

    added track

--- feed.py
+++ feed.py
@@ -1,5 +1,5 @@
 import feedparser
-print "Enter the Github username:"
+print "Enter the Github username to track:"
 uname = raw_input()
 a = feedparser.parse("http://github.com/%s.atom"%uname)
 for activity in a.entries:
